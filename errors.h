37a5264d733b60efa53c5cf39c112d73a05b1e95
Kevin Todd <kbt18@edge26.doc.ic.ac.uk>
2018-12-07 11:48:32 +0000

    created error file

---
+++ errors.h
@@ -0,0 +1,8 @@
+/* error codes */
+#define INVALID_BOARD_POSITION          1
+#define SAME_DESTINATION_AS_SOURCE      2
+#define NO_PIECE_AT_SOURCE              3
+#define NOT_BLACK_TURN                  4
+#define NOT_WHITE_TURN                  5
+#define ILLEGAL_MOVE                    6
+#define TAKING_OWN_PIECE                7
